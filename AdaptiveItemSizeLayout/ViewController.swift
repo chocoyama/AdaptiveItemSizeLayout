c4b30cc870e2d5cdded8a10cd891d407fc09414f
chocoyama <puripuripuririnrinririn@gmail.com>
2016-03-30 23:39:23 +0900

    Initial Commit

---
+++ AdaptiveItemSizeLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveItemSizeLayout
+//
+//  Created by 横山 拓也 on 2016/03/30.
+//  Copyright © 2016年 Takuya Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
